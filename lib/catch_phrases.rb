f16994d25f93cd7b587ca3d954cb00290bcd882f
Samuel Escobar <sescobar8714@gmail.com>
2020-01-22 00:31:51 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,3 +6,8 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link 
+  puts phrase = "It's Dangerous To Go Alone! Take This.\n"
+end
+
